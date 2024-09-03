173dc293819d33342d04efefeae602bb9b95d2cc
rutra <arturkummer08@gmail.com>
2024-09-03 15:59:02 +0200

    boom, fountain

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+from particleł import ParticleSystem
+import random
 
 class Game:
     def __init__(self, width, height):
@@ -6,6 +8,7 @@ class Game:
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
+        self.unityparticlesystem = ParticleSystem()
 
     def run(self):
         while True:
@@ -14,6 +17,13 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+
+            self.unityparticlesystem.add_particle(400, 300, random.uniform(-1, 1), -5, 1, 1000, 10, 255, 255, 255, 100, 'square')
+            self.unityparticlesystem.apply_force_to_all(0, 0.1)
+
+            self.unityparticlesystem.update()
+
+            self.unityparticlesystem.draw(self.screen)
 
             pygame.display.flip()
             self.clock.tick(60)
