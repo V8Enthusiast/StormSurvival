2a431bfa38f90cbe4bc626dae52bfe17618e38dd
rutra <arturkummer08@gmail.com>
2024-09-08 16:56:13 +0200

    made slider little better

--- classes/slider.py
+++ classes/slider.py
@@ -2,6 +2,7 @@ import pygame
 
 class Slider:
     def __init__(self, x, y, width, height, min_val, max_val, initial_val, app):
+        y = y + height
         self.rect = pygame.Rect(x, y, width, height)
         self.min_val = min_val
         self.max_val = max_val
@@ -13,9 +14,9 @@ class Slider:
         return self.rect
 
     def render(self):
-        pygame.draw.rect(self.app.screen, (200, 200, 200), self.rect)
-        handle_x = self.rect.x + (self.value - self.min_val) / (self.max_val - self.min_val) * self.rect.width
-        pygame.draw.circle(self.app.screen, (100, 100, 100), (int(handle_x), self.rect.centery), self.rect.height // 2)
+        pygame.draw.rect(self.app.screen, (50, 50, 50), self.rect, border_radius=self.rect.height // 2)
+        handle_x = self.rect.x + (self.value - self.min_val +0.05) / (self.max_val - self.min_val+0.10) * self.rect.width
+        pygame.draw.circle(self.app.screen, (10, 200, 10), (int(handle_x), self.rect.centery), self.rect.height // 2)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos):
