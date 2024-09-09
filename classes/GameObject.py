5f7e133fead89f29696ae308bb5eb8174f16e93a
V8Enthusiast <122159672+V8Enthusiast@users.noreply.github.com>
2024-09-09 12:57:46 +0200

    Player class

--- classes/GameObject.py
+++ classes/GameObject.py
@@ -1,9 +1,10 @@
 import pygame.image
-
+import pygame
 
 class GameObject():
     def __init__(self,game,x,y,w,h,image_path,visible):
         self.game=game
+        self.screen = self.game.screen
         self.x=x
         self.y=y
         self.w=w
@@ -17,5 +18,16 @@ class GameObject():
         self.x+=self.game.dx
         self.y+=self.game.dy
         self.rect=pygame.Rect(self.x,self.y,self.w,self.h)
-        self.game.screen.blit(self.image,self.rect)
+        self.screen.blit(self.image,self.rect)
+
+class Player(GameObject):
+    def __init__(self, game,x,y,w,h,image_path,visible):
+        super().__init__(game,x,y,w,h,image_path,visible)
+
+        self.health = 100
+        self.size = 25
+        self.color = (255, 105, 55)
+    def render(self):
+        self.rect=pygame.Rect(self.x,self.y,self.w,self.h)
+        self.screen.blit(self.image,self.rect)
 
