27e30e4afb63cdb3e2e42ef27878bf9ae3b99201
Jeicam <mzdanowiczm@gmail.com>
2024-09-10 11:45:29 +0200

    ababababa

--- images.py
+++ images.py
@@ -1,9 +1,9 @@
 import pygame
 
 storm = pygame.image.load("Assets/Bez-nazwy (1).jpg")
-player = pygame.image.load("Assets/player.png")
+player = pygame.image.load("Assets/ludzik.png")
 chest = pygame.image.load("Assets/Chest.jpeg")
-grass = pygame.image.load("Assets/tile.png")
-water = pygame.image.load("Assets/water.png")
+grass = pygame.image.load("Assets/Trawa3.jpg")
+water = pygame.image.load("Assets/Woda.jpg")
 gun = pygame.image.load("Assets/gun.png")
 damagedplayer = pygame.image.load("Assets/damagedplayer.png")
