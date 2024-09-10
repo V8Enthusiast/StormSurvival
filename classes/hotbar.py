c833ba2de9ffce6e571998646dbf40c4f7c0f524
rutra <arturkummer08@gmail.com>
2024-09-10 10:13:22 +0200

    kinda gun image

--- classes/hotbar.py
+++ classes/hotbar.py
@@ -1,4 +1,5 @@
 import pygame
+import images
 
 class Hotbar:
     def __init__(self, game, num_slots=5):
@@ -18,8 +19,12 @@ class Hotbar:
             color = (255, 255, 255) if i == self.selected_slot else (100, 100, 100)
             pygame.draw.rect(self.game.screen, color, (x, y, self.slot_width, self.slot_height), 2)
             if self.items[i]:
-                item_text = self.font.render(self.items[i], True, (255, 255, 255))
-                self.game.screen.blit(item_text, (x + 5, y + 5))
+                if self.items[i] == "Gun":
+                    gun_image_scaled = pygame.transform.scale(images.gun, (self.slot_width - 10, self.slot_height - 10))
+                    self.game.screen.blit(gun_image_scaled, (x + 5, y + 5))
+                else:
+                    item_text = self.font.render(self.items[i], True, (255, 255, 255))
+                    self.game.screen.blit(item_text, (x + 5, y + 5))
 
     def select_slot(self, slot):
         if 0 <= slot < self.num_slots:
