8a57783c0e094eae93df2c7077e09171c47089e3
rutra <arturkummer08@gmail.com>
2024-09-10 16:40:50 +0200

    added different damage for each gun

--- classes/weapon.py
+++ classes/weapon.py
@@ -5,7 +5,7 @@ import images
 from classes import particles
 
 class Weapon:
-    def __init__(self, game, player, image, ammo, max_ammo, fire_rate, firemode):
+    def __init__(self, game, player, image, ammo, max_ammo, fire_rate, firemode, damage):
         self.player = player
         self.image = image
         self.ammo = ammo
@@ -13,6 +13,7 @@ class Weapon:
         self.game = game
         self.fire_rate = fire_rate # cannot be 0
         self.firemode = firemode # 0 - Semi Auto, 1 - Full Auto, 2 - Burst
+        self.damage = damage
 
 
     def shoot(self):
@@ -31,7 +32,7 @@ class Weapon:
                 red, green, blue = 255, 255, 0
                 alpha = 255
                 shape = 'circle'
-                self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red, green, blue, alpha, shape)
+                self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red, green, blue, alpha, shape, self.damage)
 
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
@@ -42,11 +43,11 @@ class Weapon:
 
 class Glock17(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.glock17, 17, 17, 3, 0)
+        super().__init__(game, player, images.glock17, 17, 17, 3, 0, 10)
 
 class PumpActionShotgun(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.pump_action_shotgun, 5, 5, 2, 0)
+        super().__init__(game, player, images.pump_action_shotgun, 5, 5, 2, 0, 1)
 
     def shoot(self):
         if self.ammo > 0:
@@ -65,13 +66,13 @@ class PumpActionShotgun(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 
 class AmmoBox(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.ammo_box, 0, 0, 1, 1)
+        super().__init__(game, player, images.ammo_box, 0, 0, 1, 1, 0)
 
     def shoot(self):
         pass
@@ -81,7 +82,7 @@ class AmmoBox(Weapon):
 
 class M4A1(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.m4a1, 30, 30, 6, 1)
+        super().__init__(game, player, images.m4a1, 30, 30, 6, 1, 20)
 
     def shoot(self):
         if self.ammo > 0:
@@ -100,12 +101,12 @@ class M4A1(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 class BoltActionSniper(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.bolt_action_sniper, 5, 5, 1, 0)
+        super().__init__(game, player, images.bolt_action_sniper, 5, 5, 1, 0, 200)
 
     def shoot(self):
         if self.ammo > 0:
@@ -124,13 +125,13 @@ class BoltActionSniper(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 
 class AmmoCrate(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.ammo_crate, 0, 0, 1, 1)
+        super().__init__(game, player, images.ammo_crate, 0, 0, 1, 1, 0)
 
     def shoot(self):
         pass
@@ -140,7 +141,7 @@ class AmmoCrate(Weapon):
 
 class MAC10(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.mac10, 30, 30, 14, 1)
+        super().__init__(game, player, images.mac10, 30, 30, 14, 1, 10)
 
     def shoot(self):
         if self.ammo > 0:
@@ -159,12 +160,12 @@ class MAC10(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 class M1911(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.m1911, 7, 7, 4, 0)
+        super().__init__(game, player, images.m1911, 7, 7, 4, 0, 15)
 
     def shoot(self):
         if self.ammo > 0:
@@ -183,12 +184,12 @@ class M1911(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 class ScarH(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.scarh, 20, 20, 5, 1)
+        super().__init__(game, player, images.scarh, 20, 20, 5, 1, 27)
 
     def shoot(self):
         if self.ammo > 0:
@@ -207,12 +208,12 @@ class ScarH(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 class DesertEagle(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.desert_eagle, 7, 7, 3, 0)
+        super().__init__(game, player, images.desert_eagle, 7, 7, 3, 0, 34)
 
     def shoot(self):
         if self.ammo > 0:
@@ -231,12 +232,12 @@ class DesertEagle(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
 
 class Magnum44(Weapon):
     def __init__(self, game, player):
-        super().__init__(game, player, images.magnum44, 6, 6, 2, 0)
+        super().__init__(game, player, images.magnum44, 6, 6, 2, 0, 69)
 
     def shoot(self):
         if self.ammo > 0:
@@ -255,5 +256,5 @@ class Magnum44(Weapon):
                 alpha = 255
                 shape = 'circle'
                 self.player.game.weaponparticlesystem.add_particle(tip_x, tip_y, vx, vy, speed, lifespan, size, red,
-                                                                   green, blue, alpha, shape)
+                                                                   green, blue, alpha, shape, self.damage)
             self.game.sound_mixer.play_sound('Assets/shoot.mp3')
