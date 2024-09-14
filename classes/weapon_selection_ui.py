5531f20ad7fc418180fe64d29b566e7be03f81d7
rutra <arturkummer08@gmail.com>
2024-09-14 20:08:40 +0200

    made weapon ui not show ammothing

--- classes/weapon_selection_ui.py
+++ classes/weapon_selection_ui.py
@@ -2,6 +2,7 @@ import pygame
 
 class WeaponSelectionUI:
     def __init__(self, game, weapons, ammothing):
+        self.ammothing = ammothing
         self.game = game
         self.weapons = weapons
         self.font = pygame.font.Font(None, 36)
@@ -9,16 +10,18 @@ class WeaponSelectionUI:
         self.margin_x = 100
         self.margin_y = 50
         self.start_x = (self.game.app.width - self.calculate_total_width()) // 2
-        self.ammothing = ammothing
 
     def calculate_total_width(self):
-        return sum(weapon.image.get_width() for weapon in self.weapons) + self.margin_x * (len(self.weapons) - 1)
+        return sum(weapon.image.get_width() for weapon in self.weapons if weapon != self.ammothing) + self.margin_x * (len(self.weapons) - 1)
 
     def render(self):
         ui_surface = pygame.Surface((self.game.app.width, self.game.app.height), pygame.SRCALPHA)
         ui_surface.fill((0, 0, 0, 150))
 
         for i, weapon in enumerate(self.weapons):
+            if weapon == self.ammothing:
+                continue
+
             weapon_image = weapon.image
             image_x = self.start_x + i * (weapon_image.get_width() + self.margin_x)
             image_y = self.game.app.height // 2 - weapon_image.get_height() // 2 - self.margin_y
@@ -35,6 +38,9 @@ class WeaponSelectionUI:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_x, mouse_y = event.pos
             for i, weapon in enumerate(self.weapons):
+                if weapon == self.ammothing:
+                    continue
+
                 weapon_image = weapon.image
                 image_x = self.start_x + i * (weapon_image.get_width() + self.margin_x)
                 image_y = self.game.app.height // 2 - weapon_image.get_height() // 2 - self.margin_y
