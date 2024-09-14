3851cbfaef4d66a04e198d1e6a299cad34940d26
rutra <arturkummer08@gmail.com>
2024-09-14 18:55:43 +0200

    made text clickable, instead of keyboardable

--- classes/weapon_selection_ui.py
+++ classes/weapon_selection_ui.py
@@ -12,16 +12,19 @@ class WeaponSelectionUI:
         ui_surface.fill((0, 0, 0, 150))
 
         for i, weapon in enumerate(self.weapons):
-            weapon_text = self.font.render(f"{i + 1}. {weapon.__class__.__name__}", True, (255, 255, 255))
-            ui_surface.blit(weapon_text, (self.game.app.width//2 - weapon_text.get_width()//2, 50 + i * 40))
+            weapon_text = self.font.render(weapon.__class__.__name__, True, (255, 255, 255))
+            ui_surface.blit(weapon_text, (self.game.app.width // 2 - weapon_text.get_width() // 2, 50 + i * 40))
 
         self.game.screen.blit(ui_surface, (0, 0))
 
     def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if pygame.K_1 <= event.key <= pygame.K_9:
-                index = event.key - pygame.K_1
-                if 0 <= index < len(self.weapons):
-                    self.selected_weapon = self.weapons[index]
-                    return self.selected_weapon
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_x, mouse_y = event.pos
+            for i, weapon in enumerate(self.weapons):
+                weapon_text = self.font.render(weapon.__class__.__name__, True, (255, 255, 255))
+                text_rect = weapon_text.get_rect()
+                text_rect.topleft = (self.game.app.width // 2 - weapon_text.get_width() // 2, 50 + i * 40)
+                if text_rect.collidepoint(mouse_x, mouse_y):
+                    self.selected_weapon = weapon
+                    return weapon
         return None
